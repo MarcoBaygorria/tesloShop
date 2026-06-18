ebd0daba27097501bab8f8f208cc8a47ef1c7e64
MarcoBaygorria <baygorria91@gmail.com>
2026-06-17 21:25:54 -0400

    Paginacion

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
 import { find } from 'rxjs';
+import { PaginationDto } from '../common/dtos/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -29,8 +30,13 @@ export class ProductsService {
     }    
   }
 
-  findAll() {
-    return this.productRepository.find({});
+  findAll(paginationDto: PaginationDto) {
+    const {limit = 10, offset = 0} = paginationDto
+
+    return this.productRepository.find({
+      take: limit,
+      skip: offset,
+    });
   }
 
   async findOne(id: string) {
