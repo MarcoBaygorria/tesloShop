590ed99f600206590f28a960d0dbf43483e9df76
MarcoBaygorria <baygorria91@gmail.com>
2026-07-06 15:45:35 -0400

    ProductImage y Relaciones

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from "./product-image.entity";
 
 @Entity()
 export class Product {
@@ -45,6 +46,14 @@ export class Product {
         default: []
     })
     tag: string[] = []
+
+    //un producto, muchas imagenes | Relacion
+    @OneToMany(
+        () => ProductImage,
+        (productImage) => productImage.product,
+        {cascade: true}
+    )
+    images?: ProductImage;
 
 
     @BeforeInsert()
