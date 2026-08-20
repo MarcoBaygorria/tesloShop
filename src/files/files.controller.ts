1a318c7be95b72c53b742d89df25d383ed54785e
MarcoBaygorria <baygorria91@gmail.com>
2026-08-19 20:46:08 -0400

    configmodule

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -6,11 +6,15 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 
 import { fileFilter, fileNamer } from './helpers/index';
+import { ConfigService } from '@nestjs/config';
 
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService
+  ) {}
 
   @Get('product/:imageName')
   findProductImage(
@@ -39,7 +43,7 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
-    const secureUrl = `${file.filename}`
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
 
     return {secureUrl}
   }
